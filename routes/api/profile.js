4fb1c341cff3839c238167e06c8faa8aaa918df0
KevenMc <djeloniticsPASSWORD123>
2023-01-18 18:46:59 +0000

    add github to profile routes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -4,6 +4,8 @@ const auth = require("../../middleware/auth");
 const Profile = require("../../models/Profile");
 const User = require("../../models/User");
 const { check, validationResult } = require("express-validator");
+const request = require("request");
+const config = require("config");
 
 // @route  GET api/pofile/me
 // @desc   Get current user's profile
@@ -292,4 +294,35 @@ router.delete("/education/:edu_id", auth, async (req, res) => {
     res.status(500).send("Server error");
   }
 });
+
+// @route  GET api/pofile/github/:username
+// @desc   get user repos from github
+// @access public
+router.get("/github/:username", (req, res) => {
+  try {
+    const options = {
+      uri: `https://api.github.com/users/${
+        req.params.username
+      }/repos?per_page=5&sort=created:asc&client_id=${config.get(
+        "githubClientId"
+      )}&client_secret=${config.get("githubSecret")}`,
+      method: "GET",
+      headers: { "user-agent": "node.js" },
+    };
+
+    request(options, (error, response, body) => {
+      if (error) {
+        console.error(error);
+      }
+      if (response.statusCode !== 200) {
+        return res.status(404).json({ msg: "No Github profile found" });
+      }
+      res.json(JSON.parse(body));
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Server error");
+  }
+});
+
 module.exports = router;
