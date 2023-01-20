2aff7ade10d502865f4ff3beda442ca57a082086
KevenMc <djeloniticsPASSWORD123>
2023-01-20 13:04:26 +0000

    add front end auth validation

--- client/src/components/layout/auth/Register.js
+++ client/src/components/layout/auth/Register.js
@@ -3,9 +3,11 @@ import axios from "axios";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import { setAlert } from "../../../actions/alert";
+import { register } from "../../../actions/auth";
+
 import PropTypes from "prop-types";
 
-const Register = ({ setAlert }) => {
+const Register = ({ setAlert, register }) => {
   const [formData, setFormData] = useState({
     name: "",
     email: "",
@@ -23,7 +25,7 @@ const Register = ({ setAlert }) => {
     if (password !== password2) {
       setAlert("Passwords do not match", "danger");
     } else {
-      console.log("success");
+     register({name, email, password});
     }
   };
 
@@ -41,7 +43,7 @@ const Register = ({ setAlert }) => {
             name="name"
             value={name}
             onChange={(e) => onChange(e)}
-            required
+     //       required
           />
         </div>
 
@@ -52,6 +54,7 @@ const Register = ({ setAlert }) => {
             name="email"
             value={email}
             onChange={(e) => onChange(e)}
+      //      required
           />
           <small className="form-text">
             This site uses Gravatar so if you want a profile image, use a
@@ -65,7 +68,7 @@ const Register = ({ setAlert }) => {
             placeholder="Password"
             name="password"
             value={password}
-            minLength="6"
+         //   minLength="6"
             onChange={(e) => onChange(e)}
           />
         </div>
@@ -76,7 +79,7 @@ const Register = ({ setAlert }) => {
             placeholder="Confirm Password"
             name="password2"
             value={password2}
-            minLength="6"
+           // minLength="6"
             onChange={(e) => onChange(e)}
           />
         </div>
@@ -91,6 +94,7 @@ const Register = ({ setAlert }) => {
 };
 Register.propTypes = {
   setAlert: PropTypes.func.isRequired,
+  register: PropTypes.func.isRequired
 };
 
-export default connect(null, { setAlert })(Register);
+export default connect(null, { setAlert, register })(Register);
